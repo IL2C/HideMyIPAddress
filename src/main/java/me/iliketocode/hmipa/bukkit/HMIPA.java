7ae75fe886f9b5097d1bb9e8d5b1c8f751e0d8b4
ILikeToCode <101745538+IL2C@users.noreply.github.com>
2023-01-25 21:17:21 +0000

    Reformat files

--- src/main/java/me/iliketocode/hmipa/bukkit/HMIPA.java
+++ src/main/java/me/iliketocode/hmipa/bukkit/HMIPA.java
@@ -1,14 +1,13 @@
 package me.iliketocode.hmipa.bukkit;
 
-import java.util.logging.Level;
-
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public class HMIPA extends JavaPlugin {
 
-	public void onEnable() {
-		getServer().getLogger().log(Level.WARNING, "[" + getDescription().getName()
-				+ "] This is a BungeeCord plugin. It cannot be loaded onto this server. Disabling plugin!");
-		getServer().getPluginManager().disablePlugin(this);
-	}
+    public void onEnable() {
+        getServer().getLogger().log(Level.WARNING, "[" + getDescription().getName() + "] This is a BungeeCord plugin. It cannot be loaded onto this server. Disabling plugin!");
+        getServer().getPluginManager().disablePlugin(this);
+    }
 }
--- src/main/java/me/iliketocode/hmipa/bungee/HMIPA.java
+++ src/main/java/me/iliketocode/hmipa/bungee/HMIPA.java
@@ -6,9 +6,9 @@ import net.md_5.bungee.api.plugin.Plugin;
 
 public class HMIPA extends Plugin {
 
-	public void onEnable() {
-		new Metrics(this, 7037);
+    public void onEnable() {
+        new Metrics(this, 7037);
 
-		getProxy().getPluginManager().registerListener(this, new Handshake());
-	}
+        getProxy().getPluginManager().registerListener(this, new Handshake());
+    }
 }
