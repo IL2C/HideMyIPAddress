e4dced190466559b7afb59f279c103a4e3746789
ILikeToCode <101745538+IL2C@users.noreply.github.com>
2023-01-25 21:14:15 +0000

    Removed "Event" from method names

--- src/main/java/me/iliketocode/hmipa/bungee/listeners/Handshake.java
+++ src/main/java/me/iliketocode/hmipa/bungee/listeners/Handshake.java
@@ -1,11 +1,5 @@
 package me.iliketocode.hmipa.bungee.listeners;
 
-import java.lang.reflect.Field;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
-import java.util.UUID;
-
 import net.md_5.bungee.api.event.PlayerHandshakeEvent;
 import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
@@ -15,79 +9,85 @@ import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 import net.md_5.bungee.netty.ChannelWrapper;
 
+import java.lang.reflect.Field;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.UUID;
+
 public class Handshake implements Listener {
 
-	@EventHandler(priority = EventPriority.LOW)
-	public void onPlayerHandshake(PlayerHandshakeEvent event) {
-		InitialHandler initialHandler = ((InitialHandler) event.getConnection());
+    @EventHandler(priority = EventPriority.LOW)
+    public void onPlayerHandshake(PlayerHandshakeEvent event) {
+        InitialHandler initialHandler = ((InitialHandler) event.getConnection());
 
-		try {
-			Field bungeeField = initialHandler.getClass().getDeclaredField("bungee");
-			bungeeField.setAccessible(true);
+        try {
+            Field bungeeField = initialHandler.getClass().getDeclaredField("bungee");
+            bungeeField.setAccessible(true);
 
-			Object BungeeCord = bungeeField.get(initialHandler);
+            Object BungeeCord = bungeeField.get(initialHandler);
 
-			Field connectionThrottleField = BungeeCord.getClass().getDeclaredField("connectionThrottle");
-			connectionThrottleField.setAccessible(true);
-			connectionThrottleField.set(BungeeCord, null);
+            Field connectionThrottleField = BungeeCord.getClass().getDeclaredField("connectionThrottle");
+            connectionThrottleField.setAccessible(true);
+            connectionThrottleField.set(BungeeCord, null);
 
-			setAddress(initialHandler, UUID.randomUUID(), null);
-		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
-			e.printStackTrace();
-		}
-	}
+            setAddress(initialHandler, UUID.randomUUID(), null);
+        } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@EventHandler(priority = -65)
-	public void onPreLoginEvent(PreLoginEvent event) {
-		try {
-			setAddress(((InitialHandler) event.getConnection()), null, InetAddress.getByName("0.0.0.0"));
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-	}
+    @EventHandler(priority = -65)
+    public void onPreLogin(PreLoginEvent event) {
+        try {
+            setAddress(((InitialHandler) event.getConnection()), null, InetAddress.getByName("0.0.0.0"));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@EventHandler(priority = -65)
-	public void onProxyPingEventStart(ProxyPingEvent event) {
-		try {
-			setAddress(((InitialHandler) event.getConnection()), null, InetAddress.getByName("0.0.0.0"));
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-	}
+    @EventHandler(priority = -65)
+    public void onProxyPingStart(ProxyPingEvent event) {
+        try {
+            setAddress(((InitialHandler) event.getConnection()), null, InetAddress.getByName("0.0.0.0"));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@EventHandler(priority = 65)
-	public void onProxyPingEventEnd(ProxyPingEvent event) {
-		setAddress(((InitialHandler) event.getConnection()), UUID.randomUUID(), null);
-	}
+    @EventHandler(priority = 65)
+    public void onProxyPingEnd(ProxyPingEvent event) {
+        setAddress(((InitialHandler) event.getConnection()), UUID.randomUUID(), null);
+    }
 
-	private void setAddress(InitialHandler initialHandler, UUID uuid, InetAddress InetAddress) {
-		try {
-			Field chField = initialHandler.getClass().getDeclaredField("ch");
-			chField.setAccessible(true);
+    private void setAddress(InitialHandler initialHandler, UUID uuid, InetAddress InetAddress) {
+        try {
+            Field chField = initialHandler.getClass().getDeclaredField("ch");
+            chField.setAccessible(true);
 
-			ChannelWrapper channelWrapper = (ChannelWrapper) chField.get(initialHandler);
+            ChannelWrapper channelWrapper = (ChannelWrapper) chField.get(initialHandler);
 
-			Field remoteAddressField = channelWrapper.getClass().getDeclaredField("remoteAddress");
-			remoteAddressField.setAccessible(true);
+            Field remoteAddressField = channelWrapper.getClass().getDeclaredField("remoteAddress");
+            remoteAddressField.setAccessible(true);
 
-			InetSocketAddress InetSocketAddress = (java.net.InetSocketAddress) remoteAddressField.get(channelWrapper);
+            InetSocketAddress InetSocketAddress = (java.net.InetSocketAddress) remoteAddressField.get(channelWrapper);
 
-			Field holderField = InetSocketAddress.getClass().getDeclaredField("holder");
-			holderField.setAccessible(true);
+            Field holderField = InetSocketAddress.getClass().getDeclaredField("holder");
+            holderField.setAccessible(true);
 
-			Object InetSocketAddressHolder = holderField.get(InetSocketAddress);
+            Object InetSocketAddressHolder = holderField.get(InetSocketAddress);
 
-			if (uuid != null) {
-				Field hostnameField = InetSocketAddressHolder.getClass().getDeclaredField("hostname");
-				hostnameField.setAccessible(true);
-				hostnameField.set(InetSocketAddressHolder, uuid.toString().replace("-", ""));
-			}
+            if (uuid != null) {
+                Field hostnameField = InetSocketAddressHolder.getClass().getDeclaredField("hostname");
+                hostnameField.setAccessible(true);
+                hostnameField.set(InetSocketAddressHolder, uuid.toString().replace("-", ""));
+            }
 
-			Field addrField = InetSocketAddressHolder.getClass().getDeclaredField("addr");
-			addrField.setAccessible(true);
-			addrField.set(InetSocketAddressHolder, InetAddress);
-		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
-			e.printStackTrace();
-		}
-	}
+            Field addrField = InetSocketAddressHolder.getClass().getDeclaredField("addr");
+            addrField.setAccessible(true);
+            addrField.set(InetSocketAddressHolder, InetAddress);
+        } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+        }
+    }
 }
