63687f4838b2b0ee2e879dd8ceb240e069e3844b
ILikeToCode <101745538+IL2C@users.noreply.github.com>
2023-01-25 19:56:30 +0000

    Added project files

---
+++ src/main/java/me/iliketocode/hmipa/bungee/HMIPA.java
@@ -0,0 +1,14 @@
+package me.iliketocode.hmipa.bungee;
+
+import me.iliketocode.hmipa.bungee.listeners.Handshake;
+import me.iliketocode.hmipa.bungee.utils.Metrics;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class HMIPA extends Plugin {
+
+	public void onEnable() {
+		new Metrics(this, 7037);
+
+		getProxy().getPluginManager().registerListener(this, new Handshake());
+	}
+}
